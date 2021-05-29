39183eecaa636a4bfbc7bb5f5cbdad1719f3b75c
Donato Calvillo <42361463+DonatoCalvillo@users.noreply.github.com>
2021-05-29 00:49:46 -0500

    Lectura/escritura/eliminacion de informacion de JSON

--- 04-Aplicacion-de-tareas/app.js
+++ 04-Aplicacion-de-tareas/app.js
@@ -3,16 +3,27 @@ require('colors');
 
 //Nuestras importaciones
 const { inquireMenu, 
-        pausa,
-        leerInput
+    pausa,
+    leerInput,
+    listadoTareasBorrar,
+    confirmar
 } = require('./libs/inquirer');
 const Tareas = require('./models/tareas');
+const { guardarDB, leerDB } = require('./libs/guardarArchivo');
 
 console.clear();
 
 const main = async () => {
     let opt = '';
     const tareas = new Tareas();
+
+    const tareasDB = leerDB();
+
+    if(tareasDB){
+        //Cargar tareas
+        tareas.cargarTareas(tareasDB);
+    }
+
     do {
         //Await le dice esperate hasta que tengamos una 
         //respuesta de la funcion porque retorna una promesa
@@ -27,11 +38,28 @@ const main = async () => {
                 console.log(desc)
                 break;
             case '2':
-                console.log(tareas._listado);
+                tareas.listaCompleta();    
+                break;
+            case '3':
+                tareas.listaCompletadas(true);    
+                break;
+            case '4':
+                tareas.listaCompletadas(false);    
+                break;
+            case '6':
+                const id = await listadoTareasBorrar(tareas.listadoTareas);  
+                const ok = await confirmar('¿Estas seguro de que desea borrarlo?');
+                if(ok !== '0'){
+                    if(ok) {
+                        tareas.borrarTarea(id);
+                        console.log('Tarea borrada adecuandamente.');
+                    }
+                }
                 break;
             default:
                 break;
         }
+        guardarDB(tareas._listado);
         await pausa();
     } while (opt !== '0');
 }
--- 04-Aplicacion-de-tareas/models/tareas.js
+++ 04-Aplicacion-de-tareas/models/tareas.js
@@ -1,18 +1,14 @@
 const Tarea = require("./tarea");
+require('colors');
 
 class Tareas {
     //Atributos
     _listado = {};
-
+    
     constructor(){
         this._listado = {};
     }
-
-    crearTarea(desc = ''){
-        const tarea = new Tarea(desc);
-        this._listado[tarea.id] = tarea;
-    }
-
+    
     get listadoTareas(){
         const listado = [];
         Object.keys(this._listado).forEach( key => {
@@ -21,6 +17,53 @@ class Tareas {
         })
         return listado;
     }
+
+    borrarTarea(id = ''){
+        if (this._listado[id]) {
+            delete this._listado[id];
+        }
+    }
+
+    crearTarea(desc = ''){
+        const tarea = new Tarea(desc);
+        this._listado[tarea.id] = tarea;
+    }
+
+    cargarTareas(tareas = {}){
+        this._listado = tareas;
+    }
+
+    listaCompleta(){
+        console.log();
+        this.listadoTareas.forEach( (tarea, indice) => {
+            let index = `${indice + 1}`.green;
+            let {descripcion,completadoEn} = tarea;
+            completadoEn = (completadoEn) ? `Completado`.green : `Pendiente`.red;
+            console.log(`${index} ${descripcion} :: ${completadoEn}`) ;
+        })
+    }
+
+    listaCompletadas(completadas = true){
+        console.log();
+        let contador = 1;
+        this.listadoTareas.forEach( (tarea) => {
+            let index = `${contador}`.green;
+            let {descripcion,completadoEn} = tarea;
+            let completadoString = (completadoEn) ? `Completado`.green : `Pendiente`.red;
+            if(completadas){
+                if(completadoEn){
+                    console.log(`${index} ${descripcion} :: ${completadoString}`) ;
+                    contador++;
+                }
+            }else{
+                if(!completadoEn){
+                    console.log(`${index} ${descripcion} :: ${completadoString}`) ;
+                    contador++;
+                }
+            }
+        })
+    }
+
 }
 
 module.exports = Tareas;
